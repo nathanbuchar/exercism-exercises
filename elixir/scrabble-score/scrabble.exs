9d563c1409f8def182b97361c4715d01f90321c3
Nathan Buchar <blasieur@gmail.com>
2017-01-26 19:09:43 -0800

    Elixir / Scrabble Score
    Initial submission.

---
+++ elixir/scrabble-score/scrabble.exs
@@ -0,0 +1,21 @@
+defmodule Scrabble do
+  @letter_scores %{
+    "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2,
+    "H" => 4, "I" => 1, "J" => 8, "K" => 5, "L" => 1, "M" => 3, "N" => 1,
+    "O" => 1, "P" => 3, "Q" => 10, "R" => 1, "S" => 1, "T" => 1, "U" => 1,
+    "V" => 4, "W" => 4, "X" => 8, "Y" => 4, "Z" => 10
+  }
+
+  @doc """
+  Calculate the scrabble score for the word.
+  """
+  @spec score(String.t) :: non_neg_integer
+  def score(word) do
+    word
+    |> String.upcase
+    |> String.replace(~r/[^a-z]/i, "")
+    |> String.codepoints
+    |> Enum.map(fn x -> Map.get(@letter_scores, x) end)
+    |> Enum.sum
+  end
+end
